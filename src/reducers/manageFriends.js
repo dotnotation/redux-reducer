de4b739ecbb0ae7b6a78f3ba8d04153f8e00bcc6
dotnotation <dotinmotionproductions@gmail.com>
2022-02-16 21:38:26 -0500

    commit tree bee34b2a7b01ece4f07df6d0eb42b06520220a6b

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            let newFriend = action.friend
+            return { friends: [...state.friends, newFriend]}
+        case "REMOVE_FRIEND":
+            let removeFriend = state.friends.findIndex(friend => friend.id === action.id)
+            return { friends: [...state.friends.slice(0, removeFriend), ...state.friends.slice(removeFriend + 1)]
+            }
+        default:
+            return state
+    }
 }
+
+
